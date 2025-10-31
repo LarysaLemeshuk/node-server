3983645b9e515a5ad5940f7487025fdfda0983ab
Larysa Lemeshuk <larusalemeshuk@gmail.com>
2025-10-31 17:33:50 +0100

    async/await

--- index.js
+++ index.js
@@ -2,15 +2,24 @@ const http = require('http');
 
 const PORT = 5002;
 
-const requestListener = (request, response) => {
-    const { url, method} = request;
-    console.log(url, method);
+const requestListener = async (request, response) => {
+  const { url } = request;
 
+  if (url === './index.html') {
+    try {
+      const data = await fs.readFile('./views/index.html', 'utf-8');
+      response.statusCode = 200;
+      response.end(data);
+    } catch (error) {
+      response.statusCode = 404;
+      response.end('Hello from server');
+    }
+  } else {
     response.statusCode = 404;
-    response.end('Hello from server')
-}
+    response.end('Hello from server');
+  }
+};
 
 const server = http.createServer(requestListener);
 
-server.listen(PORT)
-
+server.listen(PORT);
