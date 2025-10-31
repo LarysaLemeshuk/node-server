0dc739a610698ab7c8a267fd1bac0abdf27a07c8
Larysa Lemeshuk <larusalemeshuk@gmail.com>
2025-10-31 18:23:58 +0100

    POST method read user

--- index.js
+++ index.js
@@ -1,32 +1,49 @@
 const http = require('http');
-const fs = require('fs').promises;
+const fs = require('fs/promises');
 
 const PORT = 5002;
 
 const requestListener = async (request, response) => {
-  const { url } = request;
+  const { url, method } = request;
 
-  if (url === './index.html') {
-    try {
-      const data = await fs.readFile('./views/index.html', 'utf-8');
-      response.statusCode = 200;
-      response.end(data);
-    } catch (error) {
+  if (method === 'GET') {
+    if (url === './index.html') {
+      try {
+        const data = await fs.readFile('./views/index.html', 'utf-8');
+        response.statusCode = 200;
+        response.end(data);
+      } catch (error) {
+        response.statusCode = 404;
+        response.end();
+      }
+    } else if (url === './style.css') {
+      try {
+        const data = await fs.readFile('./style.css', 'utf-8');
+        response.statusCode = 200;
+        response.end(data);
+      } catch (error) {
+        response.statusCode = 404;
+        response.end();
+      }
+    } else {
       response.statusCode = 404;
       response.end();
     }
-  } else if (url === './style.css') {
-    try {
-      const data = await fs.readFile('./style.css', 'utf-8');
-      response.statusCode = 200;
-      response.end(data);
-    } catch (error) {
-      response.statusCode = 404;
-      response.end();
+  } else if (method === 'POST') {
+    if (url === './user') {
+      let jsonString = '';
+      request.on('data', (chank) => {
+        jsonString += chank;
+      });
+      request.on('end', () => {
+        const user = JSON.parse(jsonString);
+        console.log(user);
+
+       response.statusCode = 200;
+       response.end();
+
+      });
     }
-  } else {
-    response.statusCode = 404;
-    response.end();
   }
 };
 
