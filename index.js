86c89f6fab880a9114fbfcc9ab1a6c95b1d99c69
Larysa Lemeshuk <larusalemeshuk@gmail.com>
2025-11-01 16:02:07 +0100

    add yup validation

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const yup = require('yup');
+const app = express();
+
+const bodyParser = express.json(); // request.body
+
+const validationSchema = yup.object({
+  firstName: yup.string().required(),
+  lastName: yup.string().required(),
+  email: yup.string().email().required(),
+  password: yup.string().required(),
+  isSubscribed: yup.boolean().required(),
+});
 
 const PORT = 5002;
 
-app.get('/', (request, response) => {// http://localhost:5002
-response.send('Hello world')
-})
-
-app.get('/index.html', (request, response) => {
-response.status(404).send('Test /index.html')
-})
+app.post('/user', bodyParser, async (req, res, next) => {
+ try{
+    const value = await validationSchema.validate(req.body);
+    console.log(value);
+ }catch(error){
+    res.status(400).send(error.message)
+ }
+});
 
 app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`);
-})
-console.log(app);
-
+  console.log(`Server started on port ${PORT}`);
+});
 
 // шлях + метод = роут
